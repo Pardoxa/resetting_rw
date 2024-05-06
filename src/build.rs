6164acc48e4c073910b30922014a1f34d1dfed2d
Yannick Feld <yannick.feld@uol.de>
2024-05-06 11:53:32 +0200

    buildable without git

--- src/build.rs
+++ src/build.rs
@@ -1,12 +1,23 @@
 use std::process::Command;
 
+const UNKNOWN: &str = "UNKNOWN_GIT_HASH";
+
 fn main() {
     // note: add error checking yourself.
     let output = Command::new("git")
         .args(["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+        .output();
+    let git_hash = match output{
+        Ok(output) => {
+            match String::from_utf8(output.stdout){
+                Ok(git_hash) => git_hash,
+                _ => UNKNOWN.to_owned()
+            }
+        },
+        _ => {
+            UNKNOWN.to_owned()
+        }
+    };
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
     println!("cargo:rustc-env=BUILD_TIME_CHRONO={}", chrono::offset::Local::now());
 }
